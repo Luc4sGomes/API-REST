50995c796b96fd74181fd54e3f79efe20c7d6500
lucas <lucasgominhaa448@gmail.com>
2021-10-05 17:27:44 -0300

    added changes

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(request, response) {
-    response.json({
-      'tudo ok': true,
+  async index(request, response) {
+    const novoAluno = await Aluno.create({
+      nome: 'Lucas',
+      sobrenome: 'gomes',
+      email: 'lucas@gmail.com',
+      idade: '12',
+      peso: 300,
+      altura: 2.4,
     });
+    response.json(novoAluno);
   }
 }
 
